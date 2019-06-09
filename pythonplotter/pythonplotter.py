4ae0446a2911f32c6e1a502246c29bbb7bb5addd
Emmanuel <emmanuel.nicolas_github@polytechnique.org>
2019-06-09 10:45:20 +0200

    Starting pythonplotter package
    Basic structure, but can be imported.

---
+++ pythonplotter/pythonplotter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pythonplotter is a collection of helpers to produce useful plots.
+Mostly used for computational chemistry.
+"""
+
+
+# Here comes your imports
+
+# Here comes your (few) global variables
+
+# Here comes your class definitions
+
+# Here comes your function definitions
+
+
+def main():
+    """Launcher."""
+    # init the GUI or anything else
+    pass
+
+
+if __name__ == "__main__":
+    main()
