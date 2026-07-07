f0123b33b43547ab7abde7afa6a6601d65333449
Emmanuel Nicolas <1679586+ecnicolas@users.noreply.github.com>
2026-07-07 17:20:22 +0200

    Ruff on uv_plotter

--- uv_plotter.py
+++ uv_plotter.py
@@ -27,7 +27,7 @@ def extract(logfile):
     return excited_states
 
 
-def generate(excited_states, plt_range, grid_size, sigma = 0.4, normalization=True):
+def generate(excited_states, plt_range, grid_size, sigma=0.4, normalization=True):
     """Generate gaussian functions for each excitation energy, and sum them all
 
     - Generate a range of wavelengths to use to generate the spectrum
@@ -37,7 +37,7 @@ def generate(excited_states, plt_range, grid_size, sigma = 0.4, normalization=Tr
     """
 
     # Generate the range of wavelengths
-    grid = range(plt_range[0], plot_range[1]+1, grid_size)
+    grid = range(plt_range[0], plot_range[1] + 1, grid_size)
 
     # Generate data for each point
     uv_spectrum = list()
@@ -45,21 +45,35 @@ def generate(excited_states, plt_range, grid_size, sigma = 0.4, normalization=Tr
     electron_charge = 4.803204e-10
     electron_mass = 9.10938e-31
 
-    cnst = math.sqrt(math.pi) * electron_charge**2 * constants.Avogadro / ( 1000 * math.log(10) * constants.c**2 * electron_mass ) 
+    cnst = (
+        math.sqrt(math.pi)
+        * electron_charge**2
+        * constants.Avogadro
+        / (1000 * math.log(10) * constants.c**2 * electron_mass)
+    )
     cnst = 1.3062974e8
 
-    sigma = np.power(10, 9) * constants.Planck * constants.c / 1.602176634e-19 / sigma # convert to wavenumber
+    sigma = (
+        np.power(10, 9) * constants.Planck * constants.c / 1.602176634e-19 / sigma
+    )  # convert to wavenumber
 
     for point in grid:
         fit = 0.0
-        for (wv, os) in excited_states:
-            fit += cnst * os / sigma * np.exp( - np.power((10**7 / point - 10**7 / wv) / (sigma), 2))
+        for wv, os in excited_states:
+            fit += (
+                cnst
+                * os
+                / sigma
+                * np.exp(-np.power((10**7 / point - 10**7 / wv) / (sigma), 2))
+            )
         uv_spectrum += [(point, fit)]
 
     # Normalize if requested
     if normalization:
         max_energy = max(point[1] for point in uv_spectrum)  # Retrieve max energy
-        uv_spectrum = [(wavelength, energy / max_energy) for (wavelength, energy) in uv_spectrum]
+        uv_spectrum = [
+            (wavelength, energy / max_energy) for (wavelength, energy) in uv_spectrum
+        ]
 
     # Return final spectrum line
     return uv_spectrum
@@ -67,7 +81,7 @@ def generate(excited_states, plt_range, grid_size, sigma = 0.4, normalization=Tr
 
 # %%
 # List of files to plot
-files_root = Path('data/color-prediction/')
+files_root = Path("data/color-prediction/")
 files = list()
 for file in files_root.iterdir():
     if file.is_file() and file.suffix == ".log":
@@ -81,7 +95,6 @@ write_data = True
 plot_data = False
 
 for file in files:
-
     # Extract data and generate the line to plot
     data = extract(file.as_posix())
     uv_spectrum = generate(data, plot_range, plot_grid, sigma, normalization=False)
@@ -95,19 +108,18 @@ for file in files:
 
     if write_data:
         csv_file = Path(files_root, file.stem + ".csv")
-        with open(csv_file, 'w') as f:
+        with open(csv_file, "w") as f:
             writer = csv.writer(f, delimiter=";")
             writer.writerows(zip(wavelengths, absorbances))
-        with open(Path(files_root, file.stem + "_max.dat"), 'w') as f:
+        with open(Path(files_root, file.stem + "_max.dat"), "w") as f:
             f.write(file.stem + " lambda_max = " + str(lambda_max) + "\n")
-
 
     if plot_data:
         # Setup the plot
         fig, ax = plot.subplots()
 
-        ax.set_xlabel('Energy (nm)')
-        ax.set_ylabel('Normalized absorbance')
+        ax.set_xlabel("Energy (nm)")
+        ax.set_ylabel("Normalized absorbance")
         ax.set_title(file.stem)
         ax.plot(wavelengths, absorbances)
         fig.show()
@@ -115,5 +127,3 @@ for file in files:
         # Save image
         img_file = Path(files_root, file.stem + ".png")
         fig.savefig(img_file, dpi=300)
-        
-
