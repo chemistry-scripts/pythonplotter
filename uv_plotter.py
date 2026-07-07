dfd121068e2e3c7e257c2a49d539fd3bed6bb800
Emmanuel Nicolas <1679586+ecnicolas@users.noreply.github.com>
2026-07-07 20:31:04 +0200

    Add color generation. Still an issue in the code, but the general boilerplate is there

--- uv_plotter.py
+++ uv_plotter.py
@@ -3,6 +3,7 @@ import csv
 import math
 from pathlib import Path
 import numpy as np
+import pandas as pd
 
 import cclib
 import scipy.constants as constants
@@ -10,7 +11,7 @@ import matplotlib.pyplot as plot
 from cclib.parser.utils import convertor
 
 
-def extract(logfile):
+def extract_data_from_logs(logfile):
     """Extract excited state energies and oscillator strength from logfile
 
     Fill excited_states with tuples containing wavelength in nm and oscillator strength.
@@ -27,7 +28,9 @@ def extract(logfile):
     return excited_states
 
 
-def generate(excited_states, plt_range, grid_size, sigma=0.4, normalization=True):
+def generate_spectrum(
+    excited_states, plt_range, grid_size, sigma=0.4, normalization=True
+):
     """Generate gaussian functions for each excitation energy, and sum them all
 
     - Generate a range of wavelengths to use to generate the spectrum
@@ -79,6 +82,115 @@ def generate(excited_states, plt_range, grid_size, sigma=0.4, normalization=True
     return uv_spectrum
 
 
+def uvvis_to_xyz_color(wavelengths, intensities):
+    """
+    Generate xyz color coordinates according to CIE 1931 standard, using a D65 illuminant
+
+    Input: UV-Vis spectrum with wavelengths in nm and absorbance absolute values
+
+    Output: xyz data
+    """
+
+    # Setup the CIE standard, from the csv file
+    cmf = pd.read_csv("CIE-xyzs.csv", index_col=0, sep=";")
+    cmf.dropna(axis=0, inplace=True)
+    cmf = cmf.astype(float)
+    cmf.columns = ["x", "y", "z", "S"]
+
+    # Truncate the UV-Vis according to the CIE standard
+    xmin = int(min(cmf.index))
+    xmax = int(max(cmf.index))
+
+    spectrum = pd.DataFrame(np.column_stack((wavelengths, intensities)))
+    spectrum.columns = ["wavelengths", "intensities"]
+
+    truncated_spectrum = spectrum[
+        (xmin <= spectrum["wavelengths"]) & (spectrum["wavelengths"] <= xmax)
+    ]
+
+    # Convert to transmittance
+    Imin = np.min(truncated_spectrum["intensities"])
+    Imax = np.max(truncated_spectrum["intensities"])
+
+    new_I = (truncated_spectrum["intensities"] - Imin) / (Imax - Imin)
+
+    trs_I = np.power(10, -new_I)
+
+    X = np.sum(trs_I * cmf["S"] * cmf["x"])
+    Y = np.sum(trs_I * cmf["S"] * cmf["y"])
+    Z = np.sum(trs_I * cmf["S"] * cmf["z"])
+
+    den = np.sum(cmf["S"] * cmf["y"])
+
+    if den != 0.0:
+        X, Y, Z = X / den, Y / den, Z / den
+    xyz = [X, Y, Z]
+
+    return xyz
+
+
+def xyz_to_RGB(xyz):
+    """
+    Conversion of xyz coordinates into RGB coordinates.
+
+    - xyz: list: xyz coordinates.
+
+    return:
+    - np.array (3,): rgb coordinates.
+    """
+
+    xyz = np.array(xyz).reshape(-1, 1)
+
+    M = np.array(
+        [
+            [3.2410, -1.5374, -0.4986],
+            [-0.9692, 1.8760, 0.0416],
+            [0.0556, -0.2040, 1.0570],
+        ]
+    )
+
+    norm_rgb = np.dot(M, xyz)
+    norm_rgb[norm_rgb < 0] = 0
+    norm_rgb[norm_rgb > 1] = 1
+    rgb = np.round(norm_rgb * 255, 0)
+    return rgb.flatten().astype(int)
+
+
+def xyz_to_Lab(xyz):
+    """
+    Conversion of xyz coordinates into Lab coordinates.
+
+    - xyz: list: xyz coordinates.
+
+    return:
+    - np.array (3,): Lab coordinates.
+    """
+
+    def f(T, eps):
+        if T > eps:
+            return np.power(T, 1 / 3)
+        return np.power(29 / 6, 2) / 3 * T + 4 / 29
+
+    eps = np.power(6 / 29, 3)
+
+    cmf = pd.read_csv("CIE-xyzs.csv", index_col=0, sep=";")
+    cmf.dropna(axis=0, inplace=True)
+    cmf = cmf.astype(float)
+    cmf.columns = ["x", "y", "z", "S"]
+
+    Xref = np.sum(cmf["S"] * cmf["x"]) / np.sum( cmf["S"] * cmf["y"])
+    Yref = 1
+    Zref = np.sum(cmf["S"] * cmf["z"]) / np.sum( cmf["S"] * cmf["y"])
+
+
+    X, Y, Z = xyz
+    L = 116 * f(Y / Yref, eps) - 16
+    a = 500 * (f(X / Xref, eps) - f(Y / Yref, eps))
+    b = 200 * (f(Y / Yref, eps) - f(Z / Zref, eps))
+
+    return [L, a, b]
+
+
 # %%
 # List of files to plot
 files_root = Path("data/color-prediction/")
@@ -91,13 +203,16 @@ sigma = 0.4  # Broadening for Gaussians, in eV
 plot_range = [250, 800]  # Range of spectrum to display in nm
 plot_grid = 1  # Grid precision (distance between two generated points)
 
-write_data = True
+write_data = False
 plot_data = False
+generate_Lab = True
 
 for file in files:
     # Extract data and generate the line to plot
-    data = extract(file.as_posix())
-    uv_spectrum = generate(data, plot_range, plot_grid, sigma, normalization=False)
+    data = extract_data_from_logs(file.as_posix())
+    uv_spectrum = generate_spectrum(
+        data, plot_range, plot_grid, sigma, normalization=False
+    )
 
     # Reformat data
     wavelengths = [point[0] for point in uv_spectrum]
@@ -105,6 +220,8 @@ for file in files:
 
     absorbance_max_idx = np.argmax(absorbances)
     lambda_max = wavelengths[absorbance_max_idx]
+
+    print(file.stem + ": " + str(lambda_max))
 
     if write_data:
         csv_file = Path(files_root, file.stem + ".csv")
@@ -127,3 +244,8 @@ for file in files:
         # Save image
         img_file = Path(files_root, file.stem + ".png")
         fig.savefig(img_file, dpi=300)
+
+    if generate_Lab:
+        xyz = uvvis_to_xyz_color(wavelengths, absorbances)
+        rgb = xyz_to_RGB(xyz)
+        print(rgb)
